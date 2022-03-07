535c2578f1c56fa21d7ffd0564356024a0cf9ded
885685845 <96099327+885685845@users.noreply.github.com>
2022-03-07 10:54:43 +0530

    add

---
+++ c1/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Inventry} from "./components/count"
+
+function App() {
+  return (
+    <div className="App">
+     <Inventry/>
+    </div>
+  );
+}
+
+export default App;
