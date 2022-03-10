7f765385d0d4b518e80fd21824a3a9406ec3b1b5
885685845 <96099327+885685845@users.noreply.github.com>
2022-03-11 01:58:55 +0530

    add

---
+++ day-7/src/components/Forms.jsx
@@ -0,0 +1,123 @@
+import { useState,useEffect } from "react"
+import axios from "axios";
+
+export const Forms = ()=>{
+
+    const [formData, setFormData]=useState({
+        Name:"",
+        Age:"",
+        Address:"",
+        Department: "",
+        Salary:"",
+        Marital_state:"",
+    });
+
+    // useEffect(()=>{
+    //     getData();
+    // },[]);
+
+    // const getData=()=>{
+    //     axios.get("http://localhost:5001/users").then(res=>{
+    //         setFormData(res.data)
+    //     })
+    // }
+
+    const handleChange=(e)=>{
+
+        const{id, value}=e.target;
+
+        setFormData({
+            ...formData,
+            [id]:value,
+                });
+    };
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        axios.post("http://localhost:5001/users",formData).then(()=>{
+            alert("Conrulations! User stored data sucessfully.");
+            setFormData({
+                Name:"",
+                Age:"",
+                Address:"",
+                Department: "",
+                Salary:"",
+                Marital_state:"",
+
+            });
+        });
+    };
+
+    return (
+      <div>  <form onSubmit={handleSubmit}>
+            <h1>Employee Details</h1>
+           <div className="box"> <input
+             value={formData.Name}
+             id="Name"
+             type="text"
+             onChange={handleChange}
+             placeholder="Enter your name..."
+            />
+
+            <input
+             value={formData.Age}
+             id="Age"
+             type="Number"
+             onChange={handleChange}
+             placeholder="Enter your age..."
+            />
+
+            <input
+             value={formData.Address}
+             id="Address"
+             type="text"
+             onChange={handleChange}
+             placeholder="Enter your address..."
+            /> 
+
+            <input
+             value={formData.Department}
+             id="Department"
+             type="text"
+             onChange={handleChange}
+             placeholder="Enter your department..."
+            />  
+            
+           
+            
+            <input
+             value={formData.Salary}
+             id="Salary"
+             type="Number"
+             onChange={handleChange}
+             placeholder="Enter your salary..."
+            />
+
+            <input 
+             value={formData.Marital_state}
+             id="Marital_state"
+             type="text"
+             onChange={handleChange}
+             placeholder="Enter your marital status..."
+            />
+
+<input type="submit" value={"Create user"}/>
+
+            </div>
+        </form>
+
+        {/* {
+            users.map((e)=>(
+                <div>key={e.id},{e.Name}</div>
+            ))
+
+
+        } */}
+        </div>
+      
+    )
+
+
+
+}
